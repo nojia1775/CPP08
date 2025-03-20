df1344680b88e524e66ebad2074d7ddac008c017
nojia1775 <nojia.adjemrose@gmail.com>
2025-03-20 17:43:14 +0100

    ex02 fini normalement

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -29,26 +29,25 @@ int main()
 	
 	std::cout << "STD::LIST :\n";
 	std::list<int> mlist;
-	mlist.push_front(5);
-	mlist.push_front(17);
-	std::cout << "mlist.top = " << mlist.front() << std::endl;
-	mlist.pop_front();
+	mlist.push_back(5);
+	mlist.push_back(17);
+	std::cout << "mlist.top = " << mlist.back() << std::endl;
+	mlist.pop_back();
 	std::cout << "mlist.size = " << mlist.size() << std::endl;
-	mlist.push_front(3);
-	mlist.push_front(5);
-	mlist.push_front(737);
-	mlist.push_front(0);
+	mlist.push_back(3);
+	mlist.push_back(5);
+	mlist.push_back(737);
+	mlist.push_back(0);
 	std::list<int>::iterator listIt = mlist.begin();
 	std::list<int>::iterator listIte = mlist.end();
 	++listIt;
 	--listIt;
-	std::cout << "\n";
 	while (listIt != listIte)
 	{
 		std::cout << *listIt << std::endl;
 		++listIt;
 	}
 	std::list<int> sList(mlist);
-	std::cout << "sList.top = " << sList.front() << "\n\n";
+	std::cout << "sList.top = " << sList.back() << "\n\n";
 	return 0;
 }
